59d299a46aca31ee7c2131675d53d9fe47c9b5bf
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 <naradirocco.97@gmail.com>
2023-05-03 21:12:33 -0300

    presentacion 3-5

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { RouterLayout } from "./common/RouterLayout";
+import { UserMAnagementPage } from "./pages/admin/userManagement";
 import { HomePage } from "./pages/home";
 import { LoginPage } from "./pages/login";
 
@@ -8,7 +9,7 @@ export const AppRouter: React.FC<{}> = () => {
   return (
     <Routes>
       <Route path="/" element={<RouterLayout />}>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<UserMAnagementPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />}></Route>
     </Routes>
